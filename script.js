f4bbbee1c7c47444a9121bdbba9ea07c33ef6d7e
dev0lt <igorinho07@o2.pl>
2024-02-03 20:33:25 +0100

    fixed js, refined css

--- script.js
+++ script.js
@@ -3,25 +3,24 @@
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm");
 const passwordCell = document.querySelector(".password_cell");
+const errorPrompt = document.querySelectorAll(".error_prompt");
+
+errorPrompt.forEach((el) => el.style.setProperty("visibility", "hidden"));
+
+errorPrompt.forEach((el) => el.style.setProperty("color", "red"));
 
 password.classList.remove("error");
 confirmPassword.classList.remove("error");
 
-const error = document.createElement("span");
-error.style.setProperty("color", "red");
-error.style.setProperty("font-size", "0.7rem");
-error.style.setProperty("letter-spacing", "0.1rem");
-error.textContent = "*Passwords do not match";
-
 confirmPassword.onchange = function () {
+  if (!confirmPassword.value && !password.value) return;
   if (confirmPassword.value !== password.value) {
     password.classList.add("error");
     confirmPassword.classList.add("error");
-    passwordCell.removeChild(passwordCell.lastChild);
-    passwordCell.append(error);
+    errorPrompt.forEach((el) => el.style.removeProperty("visibility"));
   } else {
     password.classList.remove("error");
     confirmPassword.classList.remove("error");
-    passwordCell.removeChild(passwordCell.lastChild);
+    errorPrompt.forEach((el) => el.style.setProperty("visibility", "hidden"));
   }
 };
